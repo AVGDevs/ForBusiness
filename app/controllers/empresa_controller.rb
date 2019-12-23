1bd4e9b96d1a2c2f2838181807c5c135e8143e7e
Eryk <erykscarabello97@gmail.com>
2019-12-23 16:03:41 -0300

    Otimizando metodo de criaçao de tarefas

--- app/controllers/empresa_controller.rb
+++ app/controllers/empresa_controller.rb
@@ -46,73 +46,55 @@ class EmpresaController < AdminsBackofficeController
   def criar_tarefas_mensais
     conn = ActiveRecord::Base.connection
 
-    #Folha de pagamento
-    @empresas = conn.execute "SELECT id, CONCAT(DATE_FORMAT(STR_TO_DATE(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m'), '-' , entregaFolhaDePagamento) AS entrega_folha FROM empresas WHERE situacao_id = 1;"
+    @empresas = conn.execute "SELECT
+    id, 
+    CONCAT(DATE_FORMAT(STR_TO_DATE(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m'), '-' , entregaFolhaDePagamento) AS entrega_folha,
+    CONCAT(DATE_FORMAT(STR_TO_DATE(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m'), '-' , entregaEscritaFiscal) AS entrega_escrita,
+    CONCAT(DATE_FORMAT(STR_TO_DATE(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m'), '-' , entregaAtivoFixo) AS entrega_ativo,
+    CONCAT(DATE_FORMAT(STR_TO_DATE(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m'), '-' , entregaPgtoERecebimento) AS entrega_pgto,
+    CONCAT(DATE_FORMAT(STR_TO_DATE(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m'), '-' , entregaEstoque) AS entrega_estoque
+    FROM empresas WHERE situacao_id = 1;"
+    
+    @verificacao_de_data = conn.execute "SELECT
+    id, 
+    entregaFolhaDePagamento AS entrega_folha,
+    entregaEscritaFiscal AS entrega_escrita,
+    entregaAtivoFixo AS entrega_ativo,
+    entregaPgtoERecebimento AS entrega_pgto,
+    entregaEstoque AS entrega_estoque
+    FROM empresas WHERE situacao_id = 1;"
 
-    @tarefas = conn.execute "SELECT t.id AS id_tarefa, d.id AS id_departamento FROM departamentos AS d JOIN tarefas AS t ON t.departamento_id = d.id WHERE departamento_id = 1 AND d.situacao_id = 1 AND t.situacao_id = 1;"
+    @departamentos = conn.execute "SELECT id FROM departamentos;"
+    @mes_atual = conn.select_one "SELECT MONTH(NOW()) AS mes_atual;"
 
-    @empresas.each do |e|     
-      @tarefas.each do |t|
-        @hora_atual = conn.select_one "SELECT date_format(str_to_date(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m-%d %H:%i') AS hora_atual;"
-        empresa_tarefa = "INSERT INTO fatos (dataVencimentoTarefa, tarefa_id, empresa_id, departamento_id, status_id, created_at, updated_at) VALUES ('#{e[1]}', #{t[0]}, #{e[0]}, #{t[1]}, 2, '#{@hora_atual['hora_atual']}', '#{@hora_atual['hora_atual']}');" 
-        ActiveRecord::Base.connection.execute(empresa_tarefa);  
+    @departamentos.each do |d|
+      @empresas.each do |e| 
+        @tarefas = conn.execute "SELECT t.id AS id_tarefa, d.id AS id_departamento
+        FROM departamentos AS d 
+        JOIN tarefas AS t ON t.departamento_id = d.id 
+        WHERE d.situacao_id = 1 AND t.situacao_id = 1 AND t.departamento_id = #{d[0]};"    
+        @tarefas.each do |t|
+          @hora_atual = conn.select_one "SELECT date_format(str_to_date(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m-%d %H:%i') AS hora_atual;"
+        
+        @verificacao_de_data.each do |v|
+          verificacao_dia = e[d[0]]
+          verificacao_dia = verificacao_dia[-2, 2]
+
+          if (verificacao_dia == "31" and @mes_atual['mes_atual'] == 2) or (verificacao_dia == "30" and @mes_atual['mes_atual'] == 2) or (verificacao_dia == "29" and @mes_atual['mes_atual'] == 2)
+            @empresa_tarefa = "INSERT INTO fatos (dataVencimentoTarefa, tarefa_id, empresa_id, departamento_id, status_id, created_at, updated_at) VALUES (CONCAT(YEAR(NOW()), '-', MONTH(NOW()), '-', 28), #{t[0]}, #{e[0]}, #{t[1]}, 2, '#{@hora_atual['hora_atual']}', '#{@hora_atual['hora_atual']}');"
+          elsif (verificacao_dia == "31" and @mes_atual['mes_atual'] == 4) or (verificacao_dia == "31" and @mes_atual['mes_atual'] == 6) or (verificacao_dia == "31" and @mes_atual['mes_atual'] == 9) or (verificacao_dia == "31" and @mes_atual['mes_atual'] == 11)
+            @empresa_tarefa = "INSERT INTO fatos (dataVencimentoTarefa, tarefa_id, empresa_id, departamento_id, status_id, created_at, updated_at) VALUES (CONCAT(YEAR(NOW()), '-', MONTH(NOW()), '-', 30), #{t[0]}, #{e[0]}, #{t[1]}, 2, '#{@hora_atual['hora_atual']}', '#{@hora_atual['hora_atual']}');"            
+          else
+            @empresa_tarefa = "INSERT INTO fatos (dataVencimentoTarefa, tarefa_id, empresa_id, departamento_id, status_id, created_at, updated_at) VALUES ('#{e[d[0]]}', #{t[0]}, #{e[0]}, #{t[1]}, 2, '#{@hora_atual['hora_atual']}', '#{@hora_atual['hora_atual']}');"
+          end 
+        end  
+      ActiveRecord::Base.connection.execute(@empresa_tarefa);
       end
     end
-
-    #Escrita fiscal
-    @empresas = conn.execute "SELECT id, CONCAT(DATE_FORMAT(STR_TO_DATE(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m'), '-' , entregaEscritaFiscal) AS entrega_escrita FROM empresas WHERE situacao_id = 1;"
-
-    @tarefas = conn.execute "SELECT t.id AS id_tarefa, d.id AS id_departamento FROM departamentos AS d JOIN tarefas AS t ON t.departamento_id = d.id WHERE departamento_id = 2 AND d.situacao_id = 1 AND t.situacao_id = 1;"
-
-    @empresas.each do |e|     
-      @tarefas.each do |t|
-        @hora_atual = conn.select_one "SELECT date_format(str_to_date(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m-%d %H:%i') AS hora_atual;"
-        empresa_tarefa = "INSERT INTO fatos (dataVencimentoTarefa, tarefa_id, empresa_id, departamento_id, status_id, created_at, updated_at) VALUES ('#{e[1]}', #{t[0]}, #{e[0]}, #{t[1]}, 2, '#{@hora_atual['hora_atual']}', '#{@hora_atual['hora_atual']}');" 
-        ActiveRecord::Base.connection.execute(empresa_tarefa);  
-      end
-    end
-  
-    #Ativo
-    @empresas = conn.execute "SELECT id, CONCAT(DATE_FORMAT(STR_TO_DATE(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m'), '-' , entregaAtivoFixo) AS entrega_ativo FROM empresas WHERE situacao_id = 1;"
-
-    @tarefas = conn.execute "SELECT t.id AS id_tarefa, d.id AS id_departamento FROM departamentos AS d JOIN tarefas AS t ON t.departamento_id = d.id WHERE departamento_id = 3 AND d.situacao_id = 1 AND t.situacao_id = 1;"
-
-    @empresas.each do |e|     
-      @tarefas.each do |t|
-        @hora_atual = conn.select_one "SELECT date_format(str_to_date(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m-%d %H:%i') AS hora_atual;"
-        empresa_tarefa = "INSERT INTO fatos (dataVencimentoTarefa, tarefa_id, empresa_id, departamento_id, status_id, created_at, updated_at) VALUES ('#{e[1]}', #{t[0]}, #{e[0]}, #{t[1]}, 2, '#{@hora_atual['hora_atual']}', '#{@hora_atual['hora_atual']}');" 
-        ActiveRecord::Base.connection.execute(empresa_tarefa);  
-      end
-    end
-
-    #Pgto
-    @empresas = conn.execute "SELECT id, CONCAT(DATE_FORMAT(STR_TO_DATE(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m'), '-' , entregaPgtoERecebimento) AS entrega_pgto FROM empresas WHERE situacao_id = 1;"
-
-    @tarefas = conn.execute "SELECT t.id AS id_tarefa, d.id AS id_departamento FROM departamentos AS d JOIN tarefas AS t ON t.departamento_id = d.id WHERE departamento_id = 4 AND d.situacao_id = 1 AND t.situacao_id = 1;"
-
-    @empresas.each do |e|     
-      @tarefas.each do |t|
-        @hora_atual = conn.select_one "SELECT date_format(str_to_date(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m-%d %H:%i') AS hora_atual;"
-        empresa_tarefa = "INSERT INTO fatos (dataVencimentoTarefa, tarefa_id, empresa_id, departamento_id, status_id, created_at, updated_at) VALUES ('#{e[1]}', #{t[0]}, #{e[0]}, #{t[1]}, 2, '#{@hora_atual['hora_atual']}', '#{@hora_atual['hora_atual']}');" 
-        ActiveRecord::Base.connection.execute(empresa_tarefa);  
-      end
-    end
-
-    #Estoque
-    @empresas = conn.execute "SELECT id, CONCAT(DATE_FORMAT(STR_TO_DATE(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m'), '-' , entregaEstoque) AS entrega_estoque FROM empresas WHERE situacao_id = 1;"
-
-    @tarefas = conn.execute "SELECT t.id AS id_tarefa, d.id AS id_departamento FROM departamentos AS d JOIN tarefas AS t ON t.departamento_id = d.id WHERE departamento_id = 5 AND d.situacao_id = 1 AND t.situacao_id = 1;"
-
-    @empresas.each do |e|     
-      @tarefas.each do |t|
-        @hora_atual = conn.select_one "SELECT date_format(str_to_date(NOW(), '%Y-%m-%d %H:%i'), '%Y-%m-%d %H:%i') AS hora_atual;"
-        empresa_tarefa = "INSERT INTO fatos (dataVencimentoTarefa, tarefa_id, empresa_id, departamento_id, status_id, created_at, updated_at) VALUES ('#{e[1]}', #{t[0]}, #{e[0]}, #{t[1]}, 2, '#{@hora_atual['hora_atual']}', '#{@hora_atual['hora_atual']}');" 
-        ActiveRecord::Base.connection.execute(empresa_tarefa);  
-      end
-    end
+  end
 
   redirect_to action: "index" 
-  
-  end
+
+end
 
 end
